c296ba98e3ba738076b6b05ef964f759ee81b31b
Kadir <kadirhana91@gmail.com>
2025-10-19 15:21:13 +0000

    jjj

---
+++ resources/views/components/jet-input-error.blade.php
@@ -0,0 +1,7 @@
+@props(['for'])
+
+@error($for)
+    <div {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>
+        {{ $message }}
+    </div>
+@enderror
---
+++ resources/views/components/jet-section-border.blade.php
@@ -0,0 +1,11 @@
+<div class="hidden sm:block">
+    <div class="py-8">
+        <div class="border-t border-gray-200"></div>
+    </div>
+</div>
+
+<div class="sm:hidden">
+    <div class="py-6">
+        <div class="border-t border-gray-200"></div>
+    </div>
+</div>
